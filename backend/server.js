08e973ded3bcd151bb24e5af60801194a47fd250
lakshaygupta2004 <laksh8403@gmail.com>
2025-08-12 21:07:18 +0530

    server.js fix

--- backend/server.js
+++ backend/server.js
@@ -7,22 +7,17 @@ const authRouter = require("./src/routes/auth.route");
 const postRouter = require("./src/routes/posts.route");
 const cookieParser = require("cookie-parser");
 
-const corsOptions = {
-  origin: ['https://caption-gen-one.vercel.app'], // add dev origins too if needed
-  credentials: true,
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'], // add others if you send them
-  optionsSuccessStatus: 204
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // explicit preflight handling
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(cookieParser());
 app.use("/auth", authRouter);
 app.use("/api/post", postRouter);
+
 const PORT = process.env.PORT || 5000;
 
 dbConfig();
 app.listen(PORT, () => {
-    console.log("Server started");
+  console.log("Server started");
 });
